8b6561b0da1dd132784c70d5ec0a7bccec8105e0
Metro <66981941+ibrahimraimi@users.noreply.github.com>
2025-01-27 19:02:35 +0100

    fix: update download link

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -5,7 +5,7 @@
 		<p class="mt-4 max-w-[600px] font-bold text-lg md:text-xl lg:text-2xl">
 			Get synchronized song lyrics and sing along with your favorite songs.
 		</p>
-		<a href="https://web.crabnebula.cloud/n3rd/ohun/releases" target="_blank">
+		<a href="https://github.com/n3-rd/ohun/releases/" target="_blank">
 			<button
 				class="mt-6 rounded-full bg-[#f3c77c] px-5 py-3 text-base text-black transition-all hover:scale-110 md:text-lg lg:px-7 lg:py-4 lg:text-xl"
 			>
